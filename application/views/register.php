17656ef106064188f63be0933d29810a08236c9a
Azeem <azeemofficial10@gmail.com>
2021-07-08 13:57:51 +0530

    minor 1 word commit

--- application/views/register.php
+++ application/views/register.php
@@ -35,7 +35,7 @@
     <?php } 
     ?>
     <?php echo validation_errors('<div class="alert alert-danger">','</div>'); ?>
-    <form action="login" method="POST"> 
+    <form action="" method="POST"> 
   <label for="Username">Username:</label><br>
   <input type="text" class="form-control" name="username"><br><br>
   <label for="Email">Email:</label><br>
